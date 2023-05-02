61fc2ecb7224f8504266ffbd65d33ac5d5a4474d
Jesse Jarzynka <jesse@jessejoe.com>
2023-05-01 22:36:14 -0400

    Change from yapf to black, update layouts

--- app.py
+++ app.py
@@ -3,14 +3,19 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(page_title="VMW/AVGO Calculator")
-# Remove top padding
+
+# Remove top padding and Hide the Streamlit menu
+# https://docs.streamlit.io/knowledge-base/using-streamlit/how-hide-hamburger-menu-app
 st.markdown(
-    f'''
-    <style>
-        .block-container {{
-            padding-top: 0px;
-        }}
-</style>''', unsafe_allow_html=True)
+    """<style>
+.block-container {padding-top: 0;}
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+</style>""",
+    unsafe_allow_html=True,
+)
+
 
 @st.cache_data(ttl=300)
 def get_current_price(stock):
@@ -20,19 +25,9 @@ def get_current_price(stock):
     response.raise_for_status()
     data = response.json()
     current_price = data["chart"]["result"][0]["meta"]["regularMarketPrice"]
-    print(f'Fetched price for {stock}: {current_price}')
+    print(f"Fetched price for {stock}: {current_price}")
     return current_price
 
-
-# Hide the Streamlit menu - https://docs.streamlit.io/knowledge-base/using-streamlit/how-hide-hamburger-menu-app
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # Initalize dynamic variables
 if "total_vmw_shares" not in st.session_state:
@@ -51,74 +46,83 @@ if "vmw_fall_price" not in st.session_state:
 # Calculate the rest of the variables
 vmw_fall_url = "https://seekingalpha.com/news/3957771-vmware-potential-deal-price-break-raised-to-115-from-100-at-ubs"
 current_value = st.session_state.total_vmw_shares * st.session_state.vmw_cur_price
-potential_value = (st.session_state.total_vmw_shares *
-                   (st.session_state.percent_cash * .01) *
-                   142.5) + (st.session_state.total_vmw_shares *
-                             (st.session_state.percent_stock * .01) *
-                             (st.session_state.avgo_cur_price * 0.252))
+potential_value = (
+    st.session_state.total_vmw_shares * (st.session_state.percent_cash * 0.01) * 142.5
+) + (
+    st.session_state.total_vmw_shares
+    * (st.session_state.percent_stock * 0.01)
+    * (st.session_state.avgo_cur_price * 0.252)
+)
 potential_gain = potential_value - current_value
 potential_gain_percent = potential_gain / current_value
-avgo_break_even_price = (current_value -
-                         (st.session_state.total_vmw_shares *
-                          (st.session_state.percent_cash * .01) * 142.5)) / (
-                              st.session_state.total_vmw_shares *
-                              (st.session_state.percent_stock * .01) * 0.252)
+avgo_break_even_price = (
+    current_value
+    - (
+        st.session_state.total_vmw_shares
+        * (st.session_state.percent_cash * 0.01)
+        * 142.5
+    )
+) / (
+    st.session_state.total_vmw_shares * (st.session_state.percent_stock * 0.01) * 0.252
+)
 deal_fails_value = st.session_state.total_vmw_shares * st.session_state.vmw_fall_price
 loss_vs_current = current_value - deal_fails_value
 loss_vs_current_percent = loss_vs_current / current_value
 
 ## Layout and display elements
 with st.container():
-    st.info('Broadcom is acquiring VMWare, this calculator helps determine the risks of holding VMW stock until '
-            'after the deal closes vs. selling now')
-    
-col1, col2, col3, col4 = st.columns([1, 2, 2, 1])
+    st.info(
+        "[Broadcom is acquiring VMWare](https://www.broadcom.com/company/news/financial-releases/60271) - this calculator helps determine the risks of holding VMW stock until "
+        "after the deal closes, vs. selling now"
+    )
 
-with col2:
+col1, col2 = st.columns([2, 1])
+
+with col1:
     with st.form(key="my_form"):
-        st.number_input(
-            "Total VMW shares:",
-            key="total_vmw_shares",
-            min_value=1,
-        )
-        st.number_input(
-            "Current VMW price:",
-            key="vmw_cur_price",
-            min_value=0.01,
-            step=1.0,
-        )
-        st.number_input(
-            "Current AVGO price:",
-            key="avgo_cur_price",
-            min_value=0.01,
-            step=1.0,
-        )
-        st.divider()
-        # st.warning("These probably won't change")
-        percent_cash_input = st.number_input("% cash:",
-                                             key="percent_cash",
-                                             min_value=1,
-                                             max_value=100,
-                                             format='%d')
-        percent_stock_input = st.number_input(
-            "% stock:",
-            key="percent_stock",
-            min_value=1,
-            max_value=100,
-        )
-        st.number_input(
-            f"[VMW price if deal falls through]({vmw_fall_url}):",
-            key="vmw_fall_price",
-            min_value=0.01,
-            step=1.0,
-        )
-        submitted = st.form_submit_button(label="Submit")
+        subcol1, subcol2 = st.columns([1,1])
+        with subcol1:
+            st.number_input(
+                "Total VMW shares:",
+                key="total_vmw_shares",
+                min_value=1,
+            )
+            st.number_input(
+                "Current VMW price:",
+                key="vmw_cur_price",
+                min_value=0.01,
+                step=1.0,
+            )
+            st.number_input(
+                "Current AVGO price:",
+                key="avgo_cur_price",
+                min_value=0.01,
+                step=1.0,
+            )
+        with subcol2:
+            percent_cash_input = st.number_input(
+                "% cash:", key="percent_cash", min_value=1, max_value=100, format="%d"
+            )
+            percent_stock_input = st.number_input(
+                "% stock:",
+                key="percent_stock",
+                min_value=1,
+                max_value=100,
+            )
+            st.number_input(
+                f"[VMW price if deal falls through]({vmw_fall_url}):",
+                key="vmw_fall_price",
+                min_value=0.01,
+                step=1.0,
+            )
+
+        submitted = st.form_submit_button(label="Submit", use_container_width=True)
         if submitted:
             if (percent_cash_input + percent_stock_input) != 100:
-                st.error('% stock and % cash must equal 100')
+                st.error("% stock and % cash must equal 100")
                 st.stop()
 
-with col3:
+with col2:
     # Prefer tabular layout, but no real way to get rid of headers and can't use tooltips
     #     st.write(f'''<table>
     # <tr><td>Total current value</td><td><code>${current_value:,.2f}</code></td></tr>
@@ -128,30 +132,39 @@ with col3:
     # <tr><td>Value if deal fails</td><td><code>${deal_fails_value:,.2f}</code></td></tr>
     # <tr><td>Loss vs. current</td><td><code>${loss_vs_current:,.2f} ({loss_vs_current_percent:.2%})</code></td></tr>
     # </table>''', unsafe_allow_html=True)
-    st.markdown(f"""Total current value:  
+    st.markdown(
+        f"""Total current value:  
 `${current_value:,.2f}`""",
-                help='Current value if you sold all your VMW shares today')
+        help="Current value if you sold all your VMW shares today",
+    )
     st.markdown(
         f"""Total potential value:  
 `${potential_value:,.2f}`""",
-        help='Value of a split of AVGO and VMW if the deal closed today')
-    st.markdown(f"""Gain by waiting:  
+        help="Value of a split of AVGO and VMW if the deal closed today",
+    )
+    st.markdown(
+        f"""Gain by waiting:  
 `${potential_gain:,.2f}` (`{potential_gain_percent:.2%}`)""",
-                help='Gain (or loss) if you waited and the deal closed today')
+        help="Gain (or loss) if you waited and the deal closed today",
+    )
     st.markdown(
         f"""AVGO break even price:  
 `${avgo_break_even_price:.2f}`""",
-        help=
-        'Price AVGO would need to fall to to make it beneficial to sell before the deal closes'
+        help="Price AVGO would need to fall to to make it beneficial to sell before the deal closes",
     )
-    st.markdown(f"""Value if deal fails:  
+    st.markdown(
+        f"""Value if deal fails:  
 `${deal_fails_value:,.2f}`""",
-                help='Value of stock if deal falls through')
-    st.markdown(f"""Loss vs. current:  
+        help="Value of stock if deal falls through",
+    )
+    st.markdown(
+        f"""Loss vs. current:  
 `${loss_vs_current:,.2f}` (`{loss_vs_current_percent:.2%}`)""",
-                help="Difference between current value and value if deal fails")
+        help="Difference between current value and value if deal fails",
+    )
 
 # Doesn't seem to be an easy way to center things, so use HTML
 st.write(
     '<p style="text-align: center;"><a href="https://github.com/jessejoe/vmw-avgo-calculator">Source</a></p>',
-    unsafe_allow_html=True)
+    unsafe_allow_html=True,
+)
